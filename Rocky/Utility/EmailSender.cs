df4836b9ee928c207a0c911b590c57b040f7cd28
kazi Fazlay Rabbi <kazifazlay4@gmail.com>
2022-10-28 22:43:10 +0600

    Summary UI And Mail Send Working

---
+++ Rocky/Utility/EmailSender.cs
@@ -0,0 +1,59 @@
+﻿using Mailjet.Client;
+using Mailjet.Client.Resources;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Newtonsoft.Json.Linq;
+using System.Threading.Tasks;
+
+namespace Rocky.Utility
+{
+    public class EmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            return Execute(email, subject, htmlMessage);
+        }
+        public async Task Execute(string email, string subject, string body)
+        {
+            MailjetClient client = new MailjetClient("367d1114f20a0b01c52bb2236c54e254","ff269b9432348d01f507d00521321a39")
+            {
+                Version = ApiVersion.V3_1,
+            };
+            MailjetRequest request = new MailjetRequest
+            {
+                Resource = Send.Resource,
+            }
+             .Property(Send.Messages, new JArray {
+     new JObject {
+      {
+       "From",
+       new JObject {
+        {"Email", "daydreamer.fahim@gmail.com"},
+        {"Name", "Kazi"}
+       }
+      }, {
+       "To",
+       new JArray {
+        new JObject {
+         {
+          "Email",
+          email
+         }, {
+          "Name",
+          "DotNetMastery"
+         }
+        }
+       }
+      }, {
+       "Subject",
+       subject
+      }, {
+       "HTMLPart",
+       body
+      }
+     }
+             });
+            await client.PostAsync(request);
+        }
+    }
+    
+}
